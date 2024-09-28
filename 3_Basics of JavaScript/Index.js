79f8642207fb185c7320748f631b3001a3dbf32c
CSLearner07 <117090454+CSLearner07@users.noreply.github.com>
2024-09-29 00:14:05 +0530

    New Changes

---
+++ 3_Basics of JavaScript/Index.js
@@ -0,0 +1,71 @@
+// function getSlice(str, start, end){
+//   console.log("Original String : " , str)
+//   console.log("Sliced String : " , str.slice(start, end))
+// }
+// getSlice("Hello World", 1, 4)
+
+// const currentDate = new Date();
+// console.log(currentDate.getDate())
+
+// const currentDate = new Date();
+// console.log(currentDate.getFullYear())
+
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
+// console.log(randomNumber)
+
+// let sum = 0;
+// for (let i = 0; i < 50; i++){
+//   sum = sum + i;
+// }
+
+// console.log(sum)
+
+// function findSum(n){
+//   let sum = 0;
+//   for (let i = 0; i < n; i++){
+//     sum = sum + i;
+//   }
+//   return sum;
+// }
+
+// let n = 50;
+// console.log(findSum(n))
+
+// function twosum(a, b){
+//   return a + b;
+// }
+
+// let a = 10;
+// let b = 20;
+
+// console.log(twosum(a, b))
+
+// function square(n) {
+//   return n * n;
+// }
+
+// function sumofSquares(a, b){
+//   const val1= square(a);
+//   const val2= square(b);
+
+//   return val1 + val2;
+// }
+
+// console.log(sumofSquares(1,2))
+
+// Below is the correct example of callback function
+// function square(n){
+//   return n * n;
+// }
+// function cube(n){
+//   return n * n * n;
+// }
+
+// function sumofSomething(a, b, fn){
+//   const val1 = fn(a);
+//   const val2 = fn(b);
+
+//   return val1 + val2;
+// }
+
+// console.log(sumofSomething(1,2, square))
